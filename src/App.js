6262cbdafa13424b6d94a8dd4f23cc30e8eb8f13
melisadeniz <melisadeniz95@gmail.com>
2022-03-24 13:32:07 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from "./styles/Global";
+// router
+import { Routes, Route } from "react-router-dom";
+//context
+import MainContextProvider from "./contexts/MainContextProvider";
+//pages
+import { Home, Details } from "./pages";
+import { Footer } from "./components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainContextProvider>
+      <GlobalStyles />
+      <Routes>
+        <Route path={"/"} element={<Home />} />
+        <Route path={"/details"} element={<Details />} />
+      </Routes>
+      <Footer />
+    </MainContextProvider>
   );
 }
 
