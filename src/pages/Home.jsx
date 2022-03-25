14846694bece65d9ed47e92ee2b890f9395e48e9
melisadeniz <melisadeniz95@gmail.com>
2022-03-25 09:12:00 +0300

    params updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { useQuery } from "react-query";
 import { fetchTopArtists } from "../data";
-import Card from "../components/Card"
+import ArtistCard from "../components/ArtistCard";
 
 export default function Home() {
-
-  const topArtistsData = useQuery("discover movies", fetchTopArtists,
-    {
-      select: (data) => data.data.artists.artist,
-      retry: false,
-    }
-  );
-
+  const topArtistsData = useQuery("discover movies", fetchTopArtists, {
+    select: (data) => data.data.artists.artist,
+    retry: false,
+  });
 
   return (
     <div className="container">
       <div className="title d-flex">
         <h1>TOP ARTISTS</h1>
       </div>
-   <ul>
-      {topArtistsData?.data?.map((item) => (
-        <li>
-       <Card item={item} />
-        </li>
-      ))}
-   </ul>
-      
-        {/* {data?.map((item) => (
+      <ul>
+        {topArtistsData?.data?.map((item) => (
+          <li>
+            <ArtistCard item={item} />
+          </li>
+        ))}
+      </ul>
+
+      {/* {data?.map((item) => (
           <Card item={item} />
         ))}
       
