668d62be8dba070c4c13358564d83175ff2dc026
melisadeniz <melisadeniz95@gmail.com>
2022-03-25 14:15:37 +0300

    logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import {
   Stack,
   Image,
   useColorMode,
+  Link
 } from "@chakra-ui/react";
 import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 
@@ -17,7 +18,9 @@ export default function Navbar() {
       <Box bg={useColorModeValue("gray.100", "gray.900")} px={4}>
         <Flex h={16} alignItems={"center"} justifyContent={"space-between"}>
           <Box>
+            <Link href="/" >
             <Image src="/lastfm_logo.png" alt="logo" width={50} height={50} />
+            </Link>
           </Box>
 
           <Flex alignItems={"center"}>
