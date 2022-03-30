0b8e7b81cd76b98fb327275f928e7628ef566873
melisadeniz <melisadeniz95@gmail.com>
2022-03-30 18:37:35 +0300

    fetch optimization

--- src/components/TopArtists.jsx
+++ src/components/TopArtists.jsx
@@ -17,7 +17,7 @@ export default function TopArtists() {
   //FETCH TOP ARTISTS
   async function fetchTopArtists(pageParam = 1) {
     const response = await fetch(
-      `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${API_KEY}&format=json&page=` +
+      `http://ws.audioscrobbler.com/2.0/?method=chart.gettopartists&api_key=${API_KEY}&format=json&limit=30&page=` +
         pageParam
     );
     if (!response.ok) {
